a09ef64b043758cc1722149187729fd5d89ae362
Marcelo <marcelovqvd@gmail.com>
2023-06-13 15:49:32 -0300

    adjust

--- src/server.js
+++
@@ -1,8 +0,0 @@
-import http from 'node:http';
-
-const server = http.createServer((req, res) => {
- console.log(req);
-  res.end(String(req))
-});
-
-server.listen(3333, () => console.log('running at 3333'));
