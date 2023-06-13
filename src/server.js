b25e2964a54fc9cacdefc0a691afadb7a5c7a9b1
Marcelo <marcelovqvd@gmail.com>
2023-06-13 15:47:45 -0300

    practice

--- src/server.js
+++ src/server.js
@@ -1,36 +1,8 @@
-import http from 'node:http'
-import { randomUUID } from 'node:crypto'
-import { Database } from './database.js'
-import { json } from './middlewares/json.js'
+import http from 'node:http';
 
-const database = new Database()
+const server = http.createServer((req, res) => {
+ console.log(req);
+  res.end(String(req))
+});
 
-const server = http.createServer(async (req, res) => {
-  const { method, url } = req
-
-  await json(req, res)
-
-  if (method === 'GET' && url === '/users') {
-    const users = database.select('users')
-
-    return res.end(JSON.stringify(users))
-  }
-
-  if (method === 'POST' && url === '/users') {
-    const { name, email } = req.body
-
-    const user = {
-      id: randomUUID(),
-      name,
-      email,
-    }
-
-    database.insert('users', user)
-
-    return res.writeHead(201).end()
-  }
-
-  return res.writeHead(404).end()
-})
-
-server.listen(3333)
+server.listen(3333, () => console.log('running at 3333'));
