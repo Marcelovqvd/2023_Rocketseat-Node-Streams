6f05207147fd429fd25f9cd76ee6b95b1447da13
Marcelo <marcelovqvd@gmail.com>
2023-06-13 21:58:58 -0300

    Aplicando Streams no módulo HTTP

--- streams/fake-upload-to-stream.js
+++ streams/fake-upload-to-stream.js
@@ -1,4 +1,4 @@
-import { Readable, Writable, Transform } from 'node:stream'
+import { Readable } from 'node:stream'
 
 class OneToHundredStream extends Readable {
   index = 1
@@ -18,21 +18,7 @@ class OneToHundredStream extends Readable {
   }
 }
 
-class InverseNumberStream extends Transform {
-  _transform(chunk, encoding, callback) {
-    const transformed = Number(chunk.toString()) * -1
-
-    callback(null, Buffer.from(String(transformed)))
-  }
-}
-
-class MultiplyByTenStream extends Writable {
-  _write(chunk, encoding, callback) {
-    console.log(Number(chunk.toString()) * 10)
-    callback()
-  }
-}
-
-new OneToHundredStream()
-  .pipe(new InverseNumberStream())
-  .pipe(new MultiplyByTenStream())
+fetch('http://localhost:3334', {
+  method: 'POST',
+  body: new OneToHundredStream(),
+})
