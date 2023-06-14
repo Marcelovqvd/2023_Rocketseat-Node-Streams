17fdf1c5ed0c52b5ce1ba43760fd8f847052f88e
Marcelo <marcelovqvd@gmail.com>
2023-06-13 21:50:43 -0300

    Stream de escrita e transformação

--- streams/fake-upload-to-stream.js
+++ streams/fake-upload-to-stream.js
@@ -1,21 +1,38 @@
-import { Readable } from 'node:stream'
+import { Readable, Writable, Transform } from 'node:stream'
 
 class OneToHundredStream extends Readable {
- index = 1
+  index = 1
 
- _read() {
-  const i = this.index++
+  _read() {
+    const i = this.index++
 
-  setTimeout(() => {
-   if(i > 10 ) {
-   this.push(null)
-  } else {
-   const buf = Buffer.from(String(i))
+    setTimeout(() => {
+      if (i > 100) {
+        this.push(null)
+      } else {
+        const buf = Buffer.from(String(i))
 
-   this.push(buf)
+        this.push(buf)
+      }
+    }, 1000);
   }
-  }, 2000);  
- }
 }
 
-new OneToHundredStream().pipe(process.stdout)
+class InverseNumberStream extends Transform {
+  _transform(chunk, encoding, callback) {
+    const transformed = Number(chunk.toString()) * -1
+
+    callback(null, Buffer.from(String(transformed)))
+  }
+}
+
+class MultiplyByTenStream extends Writable {
+  _write(chunk, encoding, callback) {
+    console.log(Number(chunk.toString()) * 10)
+    callback()
+  }
+}
+
+new OneToHundredStream()
+  .pipe(new InverseNumberStream())
+  .pipe(new MultiplyByTenStream())
